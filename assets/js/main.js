8959f0a72ab1084b22aac806652e8c5d4014dc1e
bimal-creatives <72511459+bimal-creatives@users.noreply.github.com>
2022-06-18 02:56:21 +0530

    changed

--- assets/js/main.js
+++ assets/js/main.js
@@ -17798,7 +17798,31 @@ function grid() {
 }
 
 
+$(document).ready(function(){
+	var mouseX, mouseY;
+	var ww = $( window ).width();
+	var wh = $( window ).height();
+	var traX, traY;
+	$(document).mousemove(function(e){
+	  mouseX = e.pageX;
+	  mouseY = e.pageY;
+	  traX = ((4 * mouseX) / 570) - 10;
+	  traY = ((4 * mouseY) / 570) + 50;
+	  
+	  $(".title").css({"background-position": traX + "%" + traY + "%"});
+	});
+  });
+  
 
-
-
+  document.addEventListener('mousemove', move);
+  function move(e){
+	  this.querySelectorAll('.bg1').forEach(layer =>{
+		  const speed = layer.getAttribute('data-speed')
+  
+		  const x = (window.innerWidth - e.pageX*speed)/120
+		  const y = (window.innerHeight - e.pageY*speed)/120
+  
+		  layer.style.transform = `translateX(${x}px) translateY(${y}px)`
+	  })
+  }
 //# sourceMappingURL=main.js.map
