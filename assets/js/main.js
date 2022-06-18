263c5abd49f0fc850f74a0be4dd9662c344a83e8
bimal-creatives <72511459+bimal-creatives@users.noreply.github.com>
2022-06-18 19:48:24 +0530

    chnaged

--- assets/js/main.js
+++ assets/js/main.js
@@ -17835,4 +17835,22 @@ $(document).ready(function(){
 //         });
 //     });
 // });
+
+
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("navbarNav");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
 //# sourceMappingURL=main.js.map
